d514c64c4a65573d2a2b11b6eda71306018ed6ba
Drkrbrts <87629999+Drkrbrts@users.noreply.github.com>
2021-09-03 16:26:29 -0500

    Initialize brodel10/20210804-222836

---
+++ src/component/friends/Friends.jsx
@@ -0,0 +1,224 @@
+import React from "react";
+import {
+  getFriends,
+  deleteFriends,
+  searchFriend,
+} from "../../services/userService";
+import SingleFriend from "./SingleFriend";
+import "rc-pagination/assets/index.css";
+// import FriendsForm from "./FriendsForm";
+import Pagination from "rc-pagination";
+class Friends extends React.Component {
+  state = {
+    friend: [],
+    current: 1,
+    searchItem: "",
+  };
+
+  onChange = (page, pageSize) => {
+    console.log(page);
+    this.setState({ current: page-- });
+
+    getFriends(page, pageSize)
+      .then(this.onGetFriendsSuccess)
+      .catch(this.onGetFriendsError);
+    console.log(page);
+  };
+
+  onSearchChange = (e) => {
+    let target = e.target;
+    let name = target.name;
+    let value = target.value;
+
+    this.setState((prevState) => {
+      let searchValue = { ...prevState.q };
+
+      searchValue[name] = value;
+
+      return { searchItem: searchValue };
+    }, this.stateChanged);
+  };
+
+  componentDidMount() {
+    getFriends(0, 3)
+      .then(this.onGetFriendsSuccess)
+      .catch(this.onGetFriendsError);
+
+    console.log("componentDidMount");
+  }
+
+  componentDidUpdate() {
+    // getFriends(0, 1)
+    //   .then(this.onGetFriendsSuccess)
+    //   .catch(this.onGetFriendsError);
+
+    console.log("componentDidUpdate");
+    // if (friendId && prevProps.match.params.id !== friendId) {
+    //   console.log("Making an ajax call on componentDidUpdate", { friendId });
+  }
+
+  onGetFriendsSuccess = (response) => {
+    console.log(response);
+    this.setState(() => {
+      return {
+        mappedFriends: response.friends.map(this.mapFriend),
+      };
+    });
+  };
+
+  onGetFriendsError = (error) => {
+    console.log(error);
+  };
+
+  onAddFriendClick = (e) => {
+    e.preventDefault();
+    this.props.history.push("/friendsform");
+  };
+
+  onFriendDelete = (frd) => {
+    // e.preventDefault();
+    console.log(frd);
+    let id = frd.id;
+    deleteFriends(id)
+      .then(this.onFriendDeleteSuccess)
+      .catch(this.onFriendDeleteError);
+  };
+
+  onFriendDeleteSuccess = (response) => {
+    console.log(response);
+  };
+
+  onFriendDeleteError = (error) => {
+    console.log(error);
+  };
+
+  onFriendEdit = (frd) => {
+    console.log(frd);
+    let id = frd.id;
+    // let payload = frd;
+    this.props.history.push("/friends/" + id + "/edit");
+    // editFriends(id, payload)
+    //   .then(this.onEditFriendSuccess)
+    //   .catch(this.onEditFriendError);
+
+    // return <FriendsForm friend={frd} />;
+  };
+
+  onEditFriendSuccess = (response) => {
+    console.log(response);
+  };
+
+  onEditFriendError = (error) => {
+    console.log(error);
+  };
+
+  mapFriend = (aFriend) => {
+    return (
+      <SingleFriend
+        key={`Friend # ${aFriend.id}`}
+        friend={aFriend}
+        onDeleteClick={this.onFriendDelete}
+        onEditClick={this.onFriendEdit}
+      ></SingleFriend>
+    );
+  };
+
+  onSearchClick = (e) => {
+    e.preventDefault();
+    console.log(e.target);
+    let pageIndex = 0;
+    let pageSize = 3;
+    let q = this.state.searchItem.search;
+    console.log(q);
+    searchFriend(pageIndex, pageSize, q)
+      .then(this.onSearchSuccess)
+      .catch(this.onSearchError);
+  };
+
+  onSearchSuccess = (response) => {
+    console.log(response);
+    this.setState(() => {
+      return { mappedFriends: response.friend.map(this.mapFriend) };
+    });
+  };
+
+  onSearchError = (error) => {
+    console.warn(error);
+  };
+
+  render() {
+    return (
+      <React.Fragment>
+        <div className="p-2 bg-light rounded-3 mb-4">
+          <div className="container">
+            <h2>Friends</h2>
+            <button
+              onClick={this.onAddFriendClick}
+              className="btn btn-primary btn-md"
+            >
+              + Friend
+            </button>
+            <div className="col-12 col-lg-auto mb-3 mb-lg-0 me-lg-3">
+              <form>
+                <input
+                  onChange={(e) => this.onSearchChange(e)}
+                  name="search"
+                  style={{
+                    display: "inline-block",
+                    width: "250px",
+                  }}
+                  type="search"
+                  className="form-control form-control-dark"
+                  placeholder="Search..."
+                  aria-label="Search"
+                />
+                <button
+                  onClick={this.onSearchClick}
+                  className="m-1"
+                  type="submit"
+                >
+                  Search
+                </button>
+              </form>
+            </div>
+            <hr />
+
+            {/* <h3 className="display-7 fw-bold">Friends</h3>
+            <button className="btn btn-primary btn-md">+ Friend</button>
+            <div>
+              <form action="/" method="get">
+                <label htmlFor="header-search">
+                  <span className="visually-hidden">Search Friends</span>
+                </label>
+                <input
+                  type="text"
+                  id="header-search"
+                  placeholder="Search Friends"
+                  name="s"
+                />
+                <button className="m-1" type="submit">
+                  Search
+                </button>
+              </form>
+            </div> */}
+            <div className="col-md-18 p-2">
+              <div className="row">{this.state.mappedFriends}</div>
+              <div className="m-2 p-2">
+                {!this.state.searchItem ? (
+                  <Pagination
+                    onChange={this.onChange}
+                    current={this.state.current}
+                    total={25}
+                    pageSize={3}
+                  />
+                ) : null}
+              </div>
+            </div>
+          </div>
+        </div>
+      </React.Fragment>
+    );
+  }
+}
+
+export default Friends;
---
+++ src/services/userService.js
@@ -0,0 +1,198 @@
+import axios from "axios";
+// import "./peopleService";
+
+let logIn = (payload) => {
+  const config = {
+    method: "POST",
+    url: "https://api.remotebootcamp.dev/api/users/login",
+    data: payload,
+    withCredentials: true,
+    crossdomain: true,
+    headers: {
+      "Content-Type": "application/json",
+      "SABIO-AUTH": "sabio-internal-00",
+    },
+  };
+
+  return axios(config);
+};
+
+let register = (payload, onSuccess, onError) => {
+  const config = {
+    method: "POST",
+    url: "https://api.remotebootcamp.dev/api/users/register",
+    data: payload,
+    withCredentials: true,
+    crossdomain: true,
+    headers: {
+      "Content-Type": "application/json",
+      "SABIO-AUTH": "sabio-internal-00",
+    },
+  };
+
+  return axios(config);
+};
+
+let logOut = () => {
+  const config = {
+    method: "GET",
+    url: "https://api.remotebootcamp.dev/api/users/logout",
+    withCredentials: true,
+    crossdomain: true,
+    headers: {
+      "Content-Type": "application/json",
+      "SABIO-AUTH": "sabio-internal-00",
+    },
+  };
+
+  return axios(config);
+};
+
+let get = () => {
+  const config = {
+    method: "GET",
+    url: "https://api.remotebootcamp.dev/api/users/current",
+    withCredentials: true,
+    crossdomain: true,
+    headers: {
+      "Content-Type": "application/json",
+      "SABIO-AUTH": "sabio-internal-00",
+    },
+  };
+
+  return axios(config);
+};
+
+let getById = (id) => {
+  const config = {
+    method: "GET",
+    url: "https://api.remotebootcamp.dev/api/users/" + id,
+    withCredentials: true,
+    crossdomain: true,
+    headers: {
+      "Content-Type": "application/json",
+      "SABIO-AUTH": "sabio-internal-00",
+    },
+  };
+
+  return axios(config);
+};
+
+let getFriends = (pageIndex, pageSize) => {
+  const config = {
+    method: "GET",
+    url: `https://api.remotebootcamp.dev/api/friends?pageIndex=${pageIndex}&pageSize=${pageSize}`,
+    withCredentials: true,
+    crossdomain: true,
+    headers: {
+      "Content-Type": "application/json",
+      "SABIO-AUTH": "sabio-internal-00",
+    },
+  };
+  return axios(config).then((response) => {
+    console.log(response);
+    return { friends: response.data.item.pagedItems };
+  });
+};
+
+let addFriends = (payload) => {
+  const config = {
+    method: "POST",
+    url: "https://api.remotebootcamp.dev/api/friends",
+    data: payload,
+    withCredentials: true,
+    crossdomain: true,
+    headers: {
+      "Content-Type": "application/json",
+      "SABIO-AUTH": "sabio-internal-00",
+    },
+  };
+  return axios(config).then((response) => {
+    console.log(response);
+    return { friend: response.data.item, ...payload };
+  });
+};
+
+let deleteFriends = (id) => {
+  const config = {
+    method: "DELETE",
+    url: "https://api.remotebootcamp.dev/api/friends/" + id,
+    withCredentials: true,
+    crossdomain: true,
+    headers: {
+      "Content-Type": "application/json",
+      "SABIO-AUTH": "sabio-internal-00",
+    },
+  };
+  return axios(config).then((response) => {
+    console.log(response);
+    return { id };
+  });
+};
+
+let editFriends = (id, payload) => {
+  const config = {
+    method: "PUT",
+    url: "https://api.remotebootcamp.dev/api/friends/" + id,
+    data: payload,
+    withCredentials: true,
+    crossdomain: true,
+    headers: {
+      "Content-Type": "application/json",
+      "SABIO-AUTH": "sabio-internal-00",
+    },
+  };
+  return axios(config);
+};
+
+let searchFriend = (pageIndex, pageSize, q) => {
+  const config = {
+    method: "GET",
+    url: `https://api.remotebootcamp.dev/api/friends/search?pageIndex=${pageIndex}&pageSize=${pageSize}&q=${q}`,
+    withCredentials: true,
+    crossdomain: true,
+    headers: {
+      "Content-Type": "application/json",
+      "SABIO-AUTH": "sabio-internal-00",
+    },
+  };
+  return axios(config).then((response) => {
+    console.log(response);
+    return { friend: response.data.item.pagedItems };
+  });
+};
+
+let getCars = (payload) => {
+  const config = {
+    method: "POST",
+    url: "https://api.remotebootcamp.dev/api/entities/cars",
+    data: payload,
+    withCredentials: true,
+    crossdomain: true,
+    headers: {
+      "Content-Type": "application/json",
+      "SABIO-AUTH": "sabio-internal-00",
+    },
+  };
+  return axios(config).then((response) => {
+    console.log(response);
+    return { id: response.data.item, ...payload };
+  });
+};
+
+export {
+  logIn,
+  register,
+  logOut,
+  get,
+  getById,
+  getFriends,
+  getCars,
+  addFriends,
+  deleteFriends,
+  editFriends,
+  searchFriend,
+}; // export all your calls here
+
+// if you had three functions to export
+// export { logIn, register, thirdFunction
