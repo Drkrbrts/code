07e3909922e6bf58490634b7b9fb4dac8f652b62
Drkrbrts <87629999+Drkrbrts@users.noreply.github.com>
2021-09-03 16:25:51 -0500

    Initialize cfaortiz/20210802-115547

---
+++ src/Components/UserLogin.jsx
@@ -0,0 +1,104 @@
+import React from "react";
+import { NavLink } from "react-router-dom";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import { login } from "../services/userServices";
+
+class LoginPage extends React.Component {
+  state = {
+    account: { email: "", password: "", tenantId: "U025GSH64FN" },
+  };
+
+  handleSubmit = (e) => {
+    console.log("submit!");
+    //call the server
+    login(this.state.account).then(this.onLoginSucces).catch(this.onLoginError);
+  };
+
+  //callback for .then()
+  onLoginSucces(response) {
+    toast.success("You have Logged in Successfully!", "Login Success");
+    console.log("response: ", response);
+  }
+
+  //callback for .catch()
+  onLoginError(response) {
+    toast.error(response, "Login failed");
+  }
+
+  formFieldChange = (e) => {
+    //console.log(e.currentTarget);
+    //console.log(e); //can work with ID as well.
+    const account = { ...this.state.account };
+    account[e.currentTarget.name] = e.currentTarget.value;
+    this.setState({ account });
+  };
+
+  // componentDidUpdate() {
+  //   console.log("updated");
+  // }
+
+  render() {
+    const { account } = this.state;
+    return (
+      <React.Fragment>
+        <div className="container p-5">
+          <div className="row p-5 m-5">
+            <div className="col-md-4"></div>
+            <form className="d-inline-block col-md-4 shadow">
+              <h5 className="text-center">Sign in</h5>
+              {/* <!-- Email input --> */}
+              <div className="form-outline mb-4">
+                <input
+                  value={account.email}
+                  onChange={this.formFieldChange}
+                  name="email"
+                  type="text"
+                  id="email"
+                  className="form-control"
+                  placeholder="Email"
+                />
+              </div>
+
+              {/* <!-- Password input --> */}
+              <div className="form-outline mb-4">
+                <input
+                  value={account.password}
+                  onChange={this.formFieldChange}
+                  name="password"
+                  type="password"
+                  id="password"
+                  className="form-control password"
+                  placeholder="password"
+                />
+              </div>
+
+              {/* <!-- 2 column grid layout for inline styling --> */}
+
+              {/* <!-- Submit button --> */}
+              <button
+                type="button"
+                className="btn btn-primary btn-block mb-4"
+                onClick={this.handleSubmit}
+              >
+                <NavLink to="/Home" style={{ color: "white" }}>
+                  Login
+                </NavLink>
+              </button>
+
+              {/* <!-- Register buttons --> */}
+              <div className="text-center">
+                <p>
+                  Not a member? <NavLink to="/Register">Register</NavLink>
+                </p>
+              </div>
+            </form>
+            <div className="col-md-4"></div>
+          </div>
+        </div>
+      </React.Fragment>
+    );
+  }
+}
+
+export default LoginPage;
---
+++ src/Components/codeChallenge/Widget.jsx
@@ -0,0 +1,104 @@
+import React from "react";
+import { postEntity } from "./entityService";
+import { ToastContainer, toast } from "react-toastify";
+
+class Widget extends React.Component {
+  state = {
+    product: { name: "", manufacturer: "", description: "", cost: 0 },
+  };
+
+  widgetFormChange = (e) => {
+    let product = { ...this.state.product };
+    if (e.currentTarget.name === "cost") {
+      product[e.currentTarget.name] = parseInt(e.currentTarget.value);
+    }
+    product[e.currentTarget.name] = e.currentTarget.value;
+    // console.log(typeof e.currentTarget.value);
+    this.setState({ product });
+  };
+
+  createClicked = () => {
+    console.log("create Firing!");
+    postEntity(this.state.product)
+      .then(this.onEntitySuccess)
+      .catch(this.onEntityError);
+  };
+
+  onEntitySuccess = (response) => {
+    console.log(response);
+    toast.success(`Widget was created itemID:${response.data.item}`);
+  };
+
+  onEntityError = (response) => {
+    console.log("error: ", response);
+    toast.error(response, "failed");
+  };
+
+  render() {
+    return (
+      <div className="container ps-10 pe-2 pt-5 pb-5">
+        <div className="row shadow">
+          <h6 className="text-center">Widget Info</h6>
+          <form>
+            {/* <!-- 2 column grid layout with text inputs for the first and last names --> */}
+
+            <div className="form-outline mb-4">
+              <input
+                type="text"
+                name="name"
+                className="form-control"
+                placeholder="Name"
+                onChange={this.widgetFormChange}
+              />
+            </div>
+
+            <div className="form-outline mb-4">
+              <input
+                name="manufacturer"
+                type="text"
+                className="form-control"
+                placeholder="manufacturer"
+                onChange={this.widgetFormChange}
+              />
+            </div>
+
+            {/* <!-- Email input --> */}
+            <div className="form-outline mb-4">
+              <input
+                name="description"
+                type="text"
+                className="form-control"
+                placeholder="Description"
+                onChange={this.widgetFormChange}
+              />
+            </div>
+
+            {/* <!-- Password input --> */}
+            <div className="form-outline mb-4">
+              <input
+                name="cost"
+                type="text"
+                className="form-control"
+                placeholder="Cost"
+                onChange={this.widgetFormChange}
+              />
+            </div>
+
+            {/* <!--Avatar URL--> */}
+
+            {/* <!-- Submit button --> */}
+
+            <button
+              type="button"
+              className="btn btn-primary btn-block register mb-4"
+              onClick={this.createClicked}
+            >
+              Create
+            </button>
+          </form>
+        </div>
+      </div>
+    );
+  }
+}
+export default Widget;
